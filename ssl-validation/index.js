96086b905b7cd0c51083134ae5d9c017878c53f9
manar <manar.zaboub@gmail.com>
2021-05-08 17:39:16 +0200

    Fix

--- ssl-validation/index.js
+++ ssl-validation/index.js
@@ -5,6 +5,9 @@ app.get('/.well-known/pki-validation/B9BD94E7B5EE979CD8BDD5A98C22C252.txt', (req
     const file = `${__dirname}/cert.txt`
     res.download(file);
 })
+app.get('/', (req, res) => {
+    console.log('running')
+})
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`)
 })
