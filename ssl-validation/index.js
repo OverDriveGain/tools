3e09e6b060086b4c8761ac974bc128fe6d25c2b0
manar <manar.zaboub@gmail.com>
2021-05-08 17:41:18 +0200

    Port

--- ssl-validation/index.js
+++ ssl-validation/index.js
@@ -1,6 +1,6 @@
 const express= require('express');
 const app = express()
-const port = 8585
+const port = 80
 app.get('/.well-known/pki-validation/B9BD94E7B5EE979CD8BDD5A98C22C252.txt', (req, res) => {
     const file = `${__dirname}/cert.txt`
     res.download(file);
